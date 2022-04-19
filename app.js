5af573df3c10b1b8b1c4271bbe51b15b050faa9b
Ogome Benjoe Ogheneruona <me@ogomebenjoe.com>
2022-04-18 22:00:56 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+document.querySelector('.button1').addEventListener('click', firstChoice);
+
+function firstChoice(){
+    return alert('You chose One');
+}
+document.querySelector('.button2').addEventListener('click', secondChoice);
+ 
+function secondChoice(){
+    return alert('You chose Two');
+}
+
+document.querySelector('.button3').addEventListener('click', thirdChoice);
+
+function thirdChoice(){
+    return alert('You chose Three');
+}
+
+document.querySelector('.button4').addEventListener('click', fourthChoice);
+
+function fourthChoice(){
+    return alert('You chose Four');
+}
+
+document.querySelector('.button5').addEventListener('click', fifthChoice);
+
+function fifthChoice(){
+    return alert('You chose Five');
+}
+
+
